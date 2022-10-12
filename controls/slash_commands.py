3badf50b8804d324c67e02a5aee86a5d59e2f703
nodlya <nadya-popova-02@mail.ru>
2022-10-12 19:28:17 +0500

    Initial commit

---
+++ controls/slash_commands.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="5688425165:AAHkyGJxmoMejLxzkj7ArReY5GxPZBFvjmk")
+# Диспетчер
+dp = Dispatcher(bot)
+
+
+#Хэндлер на команду /start
+@dp.message_handler(commands=["start"])
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
