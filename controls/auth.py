ce97c56fdcc3b3708707d184b74d11fbc6cfd4e4
nodlya <nadya-popova-02@mail.ru>
2022-11-06 23:31:37 +0500

    Added web app in telegram bot

--- controls/auth.py
+++ controls/auth.py
@@ -21,10 +21,16 @@ def help_message(message):
     bot.send_message(message.chat.id, 'текст')
 
 
+@bot.message_handler(commands=['out'])
+def help_message(message):
+    bot.send_message(message.chat.id, 'До свидания!')
+    #удаление из бд чат id
+
+
 def get_text_messages(message):
     if bool(re.match('^[А-Я][а-я]+\s+[А-Я][а-я]+\s+[А-Я][а-я]+', message.text)):
         # body = requests.get(ip,endpoint api).json() //отправляем запрос по фамилии, по ФИ, по Фамилии И.О.?
-        with open("responsible_false.json", "r") as read_file:  # заменить на requests?
+        with open("responsible_true.json", "r") as read_file:  # заменить на requests?
             data = json.loads(read_file.read())
             if data['result'] is None:
                 reply = types.InlineKeyboardMarkup()
@@ -36,7 +42,8 @@ def get_text_messages(message):
                                  "Вашего имени нет в базе данных, может, вы что-то не так ввели",
                                  reply_markup=reply)
             else:
-                bot.send_message(message.chat.id, "Здравствуйте, " + data['result']['full_name'])
+                bot.send_message(message.chat.id, "Здравствуйте, " + data['result']['full_name'],
+                                 reply_markup=webAppKeyboard())
 
     else:
         mesg = bot.send_message(message.chat.id, "Введите ещё раз в формате \"Фамилия Имя Отчество\"")
@@ -52,13 +59,14 @@ def callback_inline(call):
         bot.send_message(call.message.chat.id, 'Напишите разработчикам, чтобы вас добавили в базу данных')
 
 
-@bot.message_handler(content_types=['text'])
-def func(message):
-    if message.text == "Попробую ещё раз":
-        mesg = bot.send_message(message.chat.id, "Введите ещё раз в формате \"Фамилия Имя Отчество\"")
-        bot.register_next_step_handler(mesg, get_text_messages)
-    elif message.text == "Я уверен в правильности введенных данных":
-        bot.send_message(message.chat.id, 'Напишите разработчикам, чтобы вас добавили в базу данных')
+def webAppKeyboard():  # создание клавиатуры с webapp кнопкой
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)  # создаем клавиатуру
+    webAppTest = types.WebAppInfo(
+        "https://famous-tarsier-114ae1.netlify.app/")  # создаем webappinfo - формат хранения url
+    one_butt = types.KeyboardButton(text="Тестовая страница", web_app=webAppTest)  # создаем кнопку типа webapp
+    keyboard.add(one_butt)  # добавляем кнопки в клавиатуру
+
+    return keyboard  # возвращаем клавиатуру
 
 
 bot.polling()
